1fbe67366ef96197fb54ca4a846e7c4fec8cd69b
Elwin-Freudiger <elwin.freudiger@gmail.com>
2025-01-06 23:36:42 +0100

    Updates

---
+++ index.28400941.js
@@ -0,0 +1,3 @@
+console.log('Welcome to the Spotify Guessing Game!');
+
+//# sourceMappingURL=index.28400941.js.map
---
+++ index.88ab1087.js
@@ -0,0 +1,2 @@
+console.log("Welcome to the Spotify Guessing Game!");
+//# sourceMappingURL=index.88ab1087.js.map
